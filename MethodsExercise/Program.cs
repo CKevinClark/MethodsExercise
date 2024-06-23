0e88c37bd150fb6939dad09835d5dd7d8f288efc
Kevin Clark <kevinclarkengineer@gmail.com>
2024-06-22 20:13:24 -0400

    Methods Exercise! I got a little too deep with the first question and sorta rushed towards the end. let me know if I need to flesh this out more ;)

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,168 @@
-﻿namespace MethodsExercise
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome, Shugyosha, to this humble hole. What is your name?");
+            string name = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(name) && char.IsLetter(name[0]))
+
+            {
+                char firstChar = char.ToUpper(name[0]);
+
+                switch (firstChar)
+                {
+                    case 'A':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Leadership qualities, independence, pioneering spirit, ambitious, self-reliant...");
+                        break;
+                    case 'B':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Practicality, stability, determination, loyalty, responsibility, grounded...");
+                        break;
+                    case 'C':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Sociable, creative, expressive, optimistic, imaginative, charming...");
+                        break;
+                    case 'D':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Hardworking, disciplined, reliable, practical, methodical, systematic...");
+                        break;
+                    case 'E':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Adventurous, energetic, curious, freedom-loving, versatile, dynamic...");
+                        break;
+                    case 'F':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Loving, nurturing, compassionate, harmonious, responsible, domestic...");
+                        break;
+                    case 'G':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Introspective, analytical, philosophical, intuitive, wise, seeking knowledge...");
+                        break;
+                    case 'H':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Ambitious, authoritative, powerful, practical, materialistic, disciplined...");
+                        break;
+                    case 'I':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Idealistic, compassionate, intuitive, romantic, sensitive, humanitarian...");
+                        break;
+                    case 'J':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Artistic, creative, expressive, diplomatic, charismatic, sociable...");
+                        break;
+                    case 'K':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Dynamic, assertive, inspirational, influential, magnetic, ambitious...");
+                        break;
+                    case 'L':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Charismatic, communicative, optimistic, sociable, affectionate, joyful...");
+                        break;
+                    case 'M':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Practical, hardworking, disciplined, responsible, strong-willed, reliable...");
+                        break;
+                    case 'N':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Innovative, adventurous, independent, energetic, unconventional, freedom-loving...");
+                        break;
+                    case 'O':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Idealistic, intuitive, spiritual, compassionate, generous, humanitarian...");
+                        break;
+                    case 'P':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Compassionate, nurturing, sensitive, imaginative, artistic, empathetic...");
+                        break;
+                    case 'Q':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Charismatic, energetic, pioneering, courageous, confident, independent...");
+                        break;
+                    case 'R':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Resourceful, determined, ambitious, practical, strong-willed, resilient...");
+                        break;
+                    case 'S':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Sociable, adaptable, communicative, expressive, charming, versatile...");
+                        break;
+                    case 'T':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Hardworking, disciplined, practical, reliable, determined, ambitious...");
+                        break;
+                    case 'U':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Charismatic, optimistic, inspirational, communicative, imaginative, compassionate...");
+                        break;
+                    case 'V':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Visionary, intuitive, idealistic, humanitarian, innovative, spiritual...");
+                        break;
+                    case 'W':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Creative, expressive, imaginative, adaptable, versatile, adventurous...");
+                        break;
+                    case 'X':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Visionary, intuitive, innovative, unconventional, independent, determined...");
+                        break;
+                    case 'Y':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Idealistic, intuitive, compassionate, sensitive, imaginative, spiritual...");
+                        break;
+                    case 'Z':
+                        Console.WriteLine("Ah, I see, by the numeration of the old masters - you reveal Charismatic, adventurous, unconventional, imaginative, visionary, optimistic...");
+                        break;
+                    default:
+                        Console.WriteLine("You came so far to play games with me Shugyosha?");
+                        break;
+                }
+            }
+            else if (!string.IsNullOrEmpty(name) && char.IsDigit(name[0]))
+            {
+                Console.WriteLine("You are walking a fine line... Shugyosha.");
+            }
+            else
+            {
+                Console.WriteLine("Wrong again, Shugyosha.");
+            }
+
+            Console.WriteLine("Tell me Shugyosha, what cannot be seen, cannot be felt, \nCannot be heard, cannot be smelt, \nIt lies behind stars and under hills, \nAnd empty holes it fills \nIt comes first and follows after, \nEnds life, kills laughter.");
+
+            string answer = Console.ReadLine();
+
+            if (answer == "Darkness")
+            {
+                Console.WriteLine("I see you are already on the path Shugyasha... fair you well until our next meeting. 111, 222, 333");
+            }
+            else
+            {
+                Console.WriteLine("Your are not ready yet... Shugyasha");
+            };
+
+            Math.Add(2,3);
+            Math.Divide(5, 6);
+            Math.Multiply(7, 11);
+            Math.Subtract(12, 18);
+
+            Math.Array(5,7,12);
+        }
+
+        class Math
+        {
+            public static void Add(int num1, int num2)
+            {
+               string answer1 = Convert.ToString( num1 + num2);
+                Console.WriteLine($"{answer1}") ;
+            }
+            public static void Multiply(int num3, int num4)
+            {
+                int answer2 = num3 * num4;
+                Console.WriteLine($"{answer2}");
+            }
+            public static void Divide(double num5, double num6)
+            {
+                double answer3 = (double)num5 / (double)num6;
+                Console.WriteLine($"{answer3}");
+            }
+            public static void Subtract(int num7, int num8)
+            {
+                int answer4 = num7 - num8;
+                Console.WriteLine($"{answer4}");
+            }
+
+            public static void Array(params int[] numbers)
+            {
+                int sum = 0;
+
+                foreach(int number in numbers)
+                {
+                    sum += number; 
+                }
+                Console.WriteLine($"{sum}");
+            }
         }
     }
 }
